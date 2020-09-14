8b08803a4f9e8950a4c67ad7e7f3d71d72a8da0a
andrerodrig <andrelmarques11@gmail.com>
2020-09-14 11:42:51 -0300

    modify allowed hosts

--- ehr/settings.py
+++ ehr/settings.py
@@ -2,10 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
